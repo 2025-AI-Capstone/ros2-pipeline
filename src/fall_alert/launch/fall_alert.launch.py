f2cc94f5539c541a8f599f18c1974922c6ad4652
suwdle <suwdle1917@gmail.com>
2025-05-07 09:18:02 +0900

    UPDATE fall_alert node

---
+++ src/fall_alert/launch/fall_alert.launch.py
@@ -0,0 +1,21 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='fall_alert',
+            executable='fall_alert_node',
+            name='fall_alert_node',
+            output='screen',
+            parameters=[
+                os.path.join(
+                get_package_share_directory('fall_alert'),
+                'config',
+                'settings.yaml'
+                )
+            ],
+        )
+    ])
