fa723539a64c1f28bc7ef691264efe454d2dfb19
suwdle <suwdle1917@gmail.com>
2025-06-13 01:02:35 +0900

    UPDATE fall_alert_node.py:parameter

--- src/fall_alert/fall_alert/fall_alert_node.py
+++ src/fall_alert/fall_alert/fall_alert_node.py
@@ -27,7 +27,7 @@ class FallAlertNode(Node):
         self.alert_publisher = self.create_publisher(String, 'fall_alert/warning', 10)
 
         self.fall_history = deque()
-        self.fall_window_sec = 3
+        self.fall_window_sec = 4
         self.threshold_ratio = 0.8
         self.last_alert_time = 0
         self.alert_cooldown = 180
